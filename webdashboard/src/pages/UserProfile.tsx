01a3549421f3538141dd7ca11da16c612b6ef36b
zavidfolushi <alex.zavidfolushi21@gmail.com>
2023-11-27 19:59:30 +0200

    main dep

---
+++ webdashboard/src/pages/UserProfile.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const UserProfile: FC = () => {
+  return (
+    <div>
+      user
+    </div>
+  );
+};
+
+export default UserProfile;
---
+++ webdashboard/src/routes/index.ts
@@ -0,0 +1,13 @@
+import React from "react"
+import Dashboard from "../pages/Dashboard";
+import UserProfile from "../pages/UserProfile";
+
+export interface IRoute {
+    path: string;
+    component: React.ComponentType;
+}
+
+export const routes: IRoute[] = [
+  { path: "/", component: Dashboard},
+  { path: "/user/:email", component: UserProfile },
+];
